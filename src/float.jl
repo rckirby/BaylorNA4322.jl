c486fb5d032a81e2ba67b656fa95a53bfb90e8a1
Rob Kirby <robert.c.kirby@gmail.com>
2022-01-11 13:29:32 -0600

    Add doc strings to float

--- src/float.jl
+++ src/float.jl
@@ -7,6 +7,15 @@ struct FloatingPoint{N}
     characteristic::Int
 end
 
+"""
+    make_FP(x, N)
+
+    makes an N-digit `FloatingPoint{N}` number from some Julia floating
+    point type.  Returns a number of the form +/- M * 10^n,
+    where the mantissa satisfies 0.1 <= M < 1.
+
+    Future versions may account for overflow/underflow
+"""
 function make_FP(x, N)
     # get the number in the form x = +/- M * 10^n,
     # where 0.1 <= M < 1
@@ -24,11 +33,14 @@ end
 
 Base.show(io::IO, x::FloatingPoint) = print(io, "Fl(", x.mantissa>0 ? "" : "-", "0.", abs(x.mantissa), " x 10^", x.characteristic, ")")
 
+"Converts a FloatingPoint{N} number back to a Julia float"
 FP2float(x, N) = x.mantissa * 10.0^(x.characteristic-N)
 
-# pair of functions going back and forth between double and N-digit FP
+"Creates a pair of functions for converting to/from the FloatingPoing{N} representation."
 make_fl_float(N) = (x->make_FP(x, N), x->FP2float(x, N))
 
+
+# overload arithmetic to DTRT with FloatingPoint{N} numbers.
 import Base: +, -, *, /, ^, abs, sqrt, <,<=,>,>=,==,!=
 for op in (:+, :-, :*, :/, :^)
    @eval begin
@@ -36,7 +48,8 @@ for op in (:+, :-, :*, :/, :^)
    end
 end
 
-# Unary negation
+# Lots of other unary operations (trig functions, exp, etc) could
+# be included here but aren't.
 (-)(x::FloatingPoint{N}) where N = FloatingPoint{N}(-x.mantissa, x.characteristic)
 sqrt(x::FloatingPoint{N}) where N = make_FP(sqrt(FP2float(x,N)),N)
 abs(x::FloatingPoint{N}) where N = FloatingPoint{N}(abs(x.mantissa), x.characteristic)
