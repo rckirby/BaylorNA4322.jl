7ea50d04b5e683a3f5ae30e0dba84dd65a0be220
Rob Kirby <robert.c.kirby@gmail.com>
2022-03-16 13:50:38 -0500

    Expose neville_table

--- src/interp.jl
+++ src/interp.jl
@@ -2,7 +2,7 @@ module Interpolation
 include("neville.jl")
 include("newtlag.jl")
 include("spline.jl")
-export neville
+export neville, neville_table
 export newton_divided_diff_tableau, newton_divided_diff, newton_eval
 export compute_natural_spline, evaluate_spline, evaluate_spline_derivative
 end
