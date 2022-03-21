2e5411f089fc7b7afc6be15d1b0df3e6344dead0
Rob Kirby <robert.c.kirby@gmail.com>
2022-03-21 12:53:50 -0500

    expose newton hermite pol

--- src/interp.jl
+++ src/interp.jl
@@ -3,6 +3,6 @@ include("neville.jl")
 include("newtlag.jl")
 include("spline.jl")
 export neville, neville_table
-export newton_divided_diff_tableau, newton_divided_diff, newton_eval
+export newton_divided_diff_tableau, newton_divided_diff, newton_eval, newton_hermite_divided_diff
 export compute_natural_spline, evaluate_spline, evaluate_spline_derivative
 end
